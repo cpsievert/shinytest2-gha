d54ea6b9289d560d715dcce789874e26d63880c2
Carson <cpsievert1@gmail.com>
2023-02-05 17:05:36 -0600

    Add a screenshot expectation

--- app/tests/testthat/test-shinytest2.R
+++ app/tests/testthat/test-shinytest2.R
@@ -1,9 +1,13 @@
 library(shinytest2)
 
 test_that("{shinytest2} recording: app", {
-  app <- AppDriver$new(name = "app", height = 1000, width = 1427)
+  app <- AppDriver$new(
+    name = "app", height = 1000, width = 1427,
+    variant = platform_variant()
+  )
   app$expect_values()
   app$set_inputs(name = "carson")
   app$click("greet")
   app$expect_values()
+  app$expect_screenshot()
 })
